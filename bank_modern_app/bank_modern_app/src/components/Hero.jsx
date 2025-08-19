568e08a5de1685d9da8497f833e2f68678334847
Jennifer Onyejekwe <onyejekwejennifer@gmail.com>
2025-08-19 20:52:43 +0100

    initial commit

---
+++ bank_modern_app/bank_modern_app/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import styles from "../style"
+import { discount, robot } from "../assets"
+
+const Hero = () => (
+    <section id="home" className={`flex
+    md:flex-row flex-col ${styles.paddingY}`}>
+      <div className= {`flex-1 ${styles.flexStart} flex-col
+      xl:px-0 sm:px-16 px-6`}>
+
+      </div>
+        Hero
+    </section>
+)
+
+
+export default Hero
